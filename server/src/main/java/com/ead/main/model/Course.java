982137ee89a36b8fd59ab98b84639fe64e164913
achintha <achinthathennakoon97@gmail.com>
2022-10-24 17:48:17 +0530

    created course model

---
+++ server/src/main/java/com/ead/main/model/Course.java
@@ -0,0 +1,21 @@
+package com.ead.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    private String courseTitle;
+
+}
