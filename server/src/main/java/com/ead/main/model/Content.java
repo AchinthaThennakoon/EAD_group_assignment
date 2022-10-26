49fecf422f51529d0a90d78ecb88a81c4e4c7832
achintha <achinthathennakoon97@gmail.com>
2022-10-26 09:45:17 +0530

    created content model and repository

--- server/src/main/java/com/ead/main/model/Content.java
+++ server/src/main/java/com/ead/main/model/Content.java
@@ -1,5 +1,25 @@
 package com.ead.main.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Content {
+
+    @Id
+    private Integer contentId;
+
+    private String contentTitle;
+    private String content;
+
 
 }
