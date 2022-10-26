24428612ef6ac142481a88a878175d4938f4f5d4
achintha <achinthathennakoon97@gmail.com>
2022-10-26 09:42:05 +0530

    created Title model and repository

---
+++ server/src/main/java/com/ead/main/model/Title.java
@@ -0,0 +1,22 @@
+package com.ead.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Title {
+
+    @Id
+    private Integer id;
+
+    private String title;
+}
