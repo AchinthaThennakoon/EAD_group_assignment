6030181766c60a29626f43d0349cd30ed70422c2
achintha <achinthathennakoon97@gmail.com>
2022-10-31 20:10:56 +0530

    completed getmapping of title

--- server/src/main/java/com/ead/main/controller/TitleController.java
+++ server/src/main/java/com/ead/main/controller/TitleController.java
@@ -1,7 +1,6 @@
 package com.ead.main.controller;
 
 import com.ead.main.model.Title;
-import com.ead.main.repository.TitleRepository;
 import com.ead.main.service.TitleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,11 +17,10 @@ public class TitleController {
     @Autowired
     private TitleService titleService;
 
-    @Autowired
-    private TitleRepository titleRepository;
-
     @GetMapping("/")
     public List<Title> getTitles(){
-        return  titleRepository.findAll();
+        return titleService.getTitles();
     }
+
+
 }
