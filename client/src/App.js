b4a6b7a2791165eadf99dc6b2061a902137fce50
ChathuraRubasingha <rubasingharcpk.19@uom.lk>
2022-10-31 22:00:59 +0530

    react frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AddCourse from './Components/AddCourse';
+import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Content from './Components/Content';
+import AddContent from './Components/AddContent';
+import ContentView from './Components/ContentView';
+import UpdateCourse from './Components/UpdateCourse';
+import UpdateContent from './Components/UpdateContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/addcourse' element = {<AddCourse />} />
+        <Route path='/content' element = {<Content />} />
+        <Route path='/addcontent' element = {<AddContent />} />
+        <Route path='/contentview' element = {<ContentView />} />
+        <Route path='/updatecourse' element = {<UpdateCourse />} />
+        <Route path='/updatecontent' element = {<UpdateContent />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
